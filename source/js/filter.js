ba66124496524721815198e78985cf829e476f75
Liliia <befolife@gmail.com>
2020-12-16 03:19:17 +0300

    Доделала полностью каталог

--- source/js/filter.js
+++ source/js/filter.js
@@ -4,49 +4,60 @@
   var KEY_CODE_ESC = 27;
   var catalogFilter = document.querySelector('.catalog__filter-wrapper');
   var catalogFilterButton = document.querySelector('.catalog__filter-button');
+  var catalogFilterBox = document.querySelector('.catalog__filter-box');
   var catalogButtonClose = document.querySelector('.catalog__button-close');
   var overlay = document.querySelector('.overlay');
 
   var hideFilter = function () {
-    catalogFilter.classList.toggle('catalog__filter-wrapper--hide');
-    catalogFilterButton.classList.toggle('catalog__filter-button--show');
+    if (catalogFilter) {
+      catalogFilter.classList.toggle('catalog__filter-wrapper--hide');
+      catalogFilterButton.classList.toggle('catalog__filter-button--show');
+    }
   };
 
   hideFilter();
 
-  var onCatalogFilterButtonClick = function (evt) {
-    evt.preventDefault();
+  var toggleStateElements = function () {
     catalogFilter.classList.toggle('catalog__filter-wrapper--hide');
     catalogFilter.classList.toggle('catalog__filter-wrapper--position');
-    catalogButtonClose.classList.add('catalog__button-close--show');
+    catalogButtonClose.classList.toggle('catalog__button-close--show');
+    catalogFilterBox.classList.toggle('catalog__filter-box--clear');
     overlay.classList.toggle('overlay--show');
-    document.body.classList.toggle('overflow-hidden');
+  };
+
+  var onCatalogFilterButtonClick = function (evt) {
+    evt.preventDefault();
+    toggleStateElements();
   };
 
   var onCatalogButtonCloseClick = function (evt) {
     evt.preventDefault();
-    catalogFilter.classList.toggle('catalog__filter-wrapper--hide');
-    catalogFilter.classList.toggle('catalog__filter-wrapper--position');
-    overlay.classList.toggle('overlay--show');
-    document.body.classList.toggle('overflow-hidden');
+    toggleStateElements();
   };
 
   var onOverlayClick = function (evt) {
     evt.preventDefault();
-    onCatalogButtonCloseClick();
+    toggleStateElements();
   };
 
   var modalEscKeydown = function (evt) {
     if (evt.keyCode === KEY_CODE_ESC) {
       evt.preventDefault();
-      if (catalogFilter.classList.contains(!'catalog__filter-wrapper--hide')) {
-        onCatalogButtonCloseClick();
+      if (catalogFilter) {
+        toggleStateElements();
       }
     }
   };
 
-  catalogFilterButton.addEventListener('click', onCatalogFilterButtonClick);
-  catalogButtonClose.addEventListener('click', onCatalogButtonCloseClick);
+  var setListenersOnFilter = function () {
+    if (catalogFilter || catalogButtonClose) {
+      catalogFilterButton.addEventListener('click', onCatalogFilterButtonClick);
+      catalogButtonClose.addEventListener('click', onCatalogButtonCloseClick);
+    }
+  };
+
+  setListenersOnFilter();
+
   overlay.addEventListener('click', onOverlayClick);
   document.addEventListener('keydown', modalEscKeydown);
 })();
