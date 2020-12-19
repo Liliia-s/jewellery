870d7f500326ec84ced0c4752159e25093e4ac51
Liliia <befolife@gmail.com>
2020-12-19 23:45:31 +0300

    Внесла правки

---
+++ source/js/slider.js
@@ -0,0 +1,44 @@
+'use strict';
+
+(function () {
+  // var sliderWrapper = document.querySelector('.slider__cards-wrapper');
+  // var sliderPagination = document.querySelector('.slider__pagination-list');
+  // var sliderPrev = document.querySelector('.slider__btn--left');
+  // var sliderNext = document.querySelector('.slider__btn--right');
+
+  // var toggleSwiper = function () {
+  //   window.libSwiper.toggleSlider(sliderWrapper, {
+  //     slidesPerView: 3,
+  //     spaceBetween: 30,
+  //     slidesPerGroup: 3,
+  //     loop: true,
+  //     loopFillGroupWithBlank: true,
+  //     pagination: {
+  //       el: sliderPagination,
+  //       clickable: true,
+  //     },
+  //     navigation: {
+  //       nextEl: sliderPrev,
+  //       prevEl: sliderNext,
+  //     },
+  //   });
+  // };
+
+  // toggleSwiper();
+
+  var swiper = new Swiper('.slider__cards-wrapper', {
+    slidesPerView: 3,
+    // spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.slider__pagination-list',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.slider__btn--left',
+      prevEl: '.slider__btn--right',
+    },
+  });
+})();
