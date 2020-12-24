aff9936e12444f5cdac14d6caa652598d812394d
Liliia <befolife@gmail.com>
2020-12-24 19:58:38 +0300

    Внесение правок после проверки наставника

--- source/js/all-util.js
+++ source/js/all-util.js
@@ -4,25 +4,8 @@
   var KEY_CODE_ESC = 27;
   var overlay = document.querySelector('.overlay');
 
-  // var modalEscKeydown = function (evt, callback) {
-  //   if (evt.keyCode === KEY_CODE_ESC) {
-  //     templateFunction(evt, callback);
-  //   }
-  // };
-
-  // var onOverlayClick = function (evt, callback) {
-  //   templateFunction(evt, callback);
-  // };
-
-  // var templateFunction = function (evt, callback) {
-  //   evt.preventDefault();
-  //   callback();
-  // };
-
   window.util = {
     KEY_CODE_ESC: KEY_CODE_ESC,
     overlay: overlay,
-    // modalEscKeydown: modalEscKeydown,
-    // onOverlayClick: onOverlayClick
   };
 })();
